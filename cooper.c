7bdba9c6a16a988331c5dee12c2f2ba30d137c52
andrea96 <andrea@notebook.localdomain>
2019-02-27 17:54:30 +0100

    Riformattato il sorgente e aggiornato l'elaborato

--- cooper.c
+++ cooper.c
@@ -1,13 +1,17 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
 
 long int gcd(long int a, long int b) {
   return b == 0 ? a : gcd(b, a % b);
 }
 
+
 long int lcm(long int a, long int b) {
   return abs((a / gcd(a, b)) * b);
 }
+
 
 typedef struct t_syntaxTree {
   char nodeName[16];
@@ -15,53 +19,64 @@ typedef struct t_syntaxTree {
   struct t_syntaxTree** nodes;
 } t_syntaxTree;
 
+
 t_syntaxTree* buildTree(int first, char** tokens) {
   t_syntaxTree* tree = malloc(sizeof(t_syntaxTree));
   tree->nodes = NULL;
-  int open; //open parentheses
+  int open;
+  
   if (tokens[first][0] == '(') {
     first++;
     tree->nodesLen = 0;
     strcpy(tree->nodeName, tokens[first]);
     open = 1;
+    
     do {
       first++;
+
       if (open == 1 && tokens[first][0]!=')') {
 	tree->nodesLen++;
 	tree->nodes = realloc(tree->nodes, sizeof(t_syntaxTree*) * tree->nodesLen);
 	tree->nodes[tree->nodesLen-1] = buildTree(first, tokens);
       }
-      if (tokens[first][0] == '(') open++;
-      if (tokens[first][0] == ')') open--;
 
+      if (tokens[first][0] == '(') open++;
+
+      if (tokens[first][0] == ')') open--;
     } while (open != 0);
   }
+  
   else {
     strcpy(tree->nodeName, tokens[first]);
     tree->nodesLen = 0;
     tree->nodes = NULL;
   }
-
+  
   return tree;
 }
+
 
 t_syntaxTree* parse(char* wff) {
   char* wffSpaced = malloc(sizeof(char));
   wffSpaced[0] = wff[0];
   int j = 1;
+
   for (int i = 1; i < strlen(wff) + 1; i++) {
+
     if (wff[i - 1] == '(') {
       wffSpaced = realloc(wffSpaced, sizeof(char) * (j + 2));
       wffSpaced[j] = ' ';
       wffSpaced[j+1] = wff[i];
       j += 2;
     }
+    
     else if (wff[i + 1] == ')') {
       wffSpaced = realloc(wffSpaced, sizeof(char) * (j + 2));
       wffSpaced[j] = wff[i];
       wffSpaced[j + 1] = ' ';
       j += 2;
     }
+    
     else {
       wffSpaced = realloc(wffSpaced, sizeof(char) * (j + 1));
       wffSpaced[j] = wff[i];
@@ -79,6 +94,7 @@ t_syntaxTree* parse(char* wff) {
     tokens = realloc(tokens, sizeof(char *) * nTokens);
     tokens[nTokens - 1] = token;
   }
+  
   t_syntaxTree* syntaxTree = buildTree(0, tokens);
 
   free(wffSpaced);
@@ -86,6 +102,7 @@ t_syntaxTree* parse(char* wff) {
 
   return syntaxTree;
 }
+
 
 int getLCM(t_syntaxTree* tree, char* var) {
   if (tree->nodeName[0] == '*') {
@@ -103,9 +120,8 @@ int getLCM(t_syntaxTree* tree, char* var) {
   return l;
 }
 
+
 void normalize(t_syntaxTree* tree, char* var) {
-
-
   int lcm = getLCM(tree, var);
   int c;
 
@@ -116,7 +132,7 @@ void normalize(t_syntaxTree* tree, char* var) {
 
       for (int j=0; j<tree->nodes[i]->nodes[0]->nodesLen; j++) {
 	if (strcmp(addends[j]->nodes[1]->nodeName, var) == 0)
-	  c = atoi(addends[j]->nodes[0]->nodeName); //variable coefficient
+	  c = atoi(addends[j]->nodes[0]->nodeName);
       }
 
       for (int j=0; j<tree->nodes[i]->nodes[0]->nodesLen; j++) {
@@ -143,7 +159,7 @@ void normalize(t_syntaxTree* tree, char* var) {
 
       for (int j=0; j<tree->nodes[i]->nodes[0]->nodesLen; j++) {
 	if (strcmp(addends[j]->nodes[1]->nodeName, var) == 0)
-	  c = atoi(addends[j]->nodes[0]->nodeName); //variable coefficient
+	  c = atoi(addends[j]->nodes[0]->nodeName);
       }
 
       for (int j=0; j<tree->nodes[i]->nodes[0]->nodesLen; j++) {
@@ -184,22 +200,28 @@ void normalize(t_syntaxTree* tree, char* var) {
   sprintf(tree->nodes[tree->nodesLen-1]->nodes[1]->nodeName, "%d", lcm);
 }
 
+
 void recFree(t_syntaxTree* tree) {
   for (int i=0; i<tree->nodesLen; i++) {
     recFree(tree->nodes[i]);
   }
+
   free(tree->nodes);
   free(tree);
 }
 
+
 t_syntaxTree* recCopy(t_syntaxTree* tree) {
   t_syntaxTree* t = malloc(sizeof(t_syntaxTree));
   t->nodesLen = tree->nodesLen;
+
   if (t->nodesLen == 0)
     t->nodes = NULL;
   else
     t->nodes = malloc(sizeof(t_syntaxTree*) * t->nodesLen);
+
   strcpy(t->nodeName, tree->nodeName);
+
   for(int i=0; i<t->nodesLen; i++) {
     t->nodes[i] = recCopy(tree->nodes[i]);
   }
@@ -221,7 +243,6 @@ t_syntaxTree* minInf(t_syntaxTree* tree, char* var) {
       t_syntaxTree** addends = nTree->nodes[i]->nodes[0]->nodes;
 
       for (int j=0; j<nTree->nodes[i]->nodes[0]->nodesLen; j++) {
-
 	if (strcmp(addends[j]->nodeName, var) == 0)
 	  strcpy(nTree->nodes[i]->nodeName, "false");
 	else if (strcmp(addends[j]->nodeName, minvar) == 0)
@@ -230,6 +251,7 @@ t_syntaxTree* minInf(t_syntaxTree* tree, char* var) {
 
       for (int j=0; j<nTree->nodes[i]->nodesLen; j++)
 	recFree(nTree->nodes[i]->nodes[j]);
+
       free(nTree->nodes[i]->nodes);
       nTree->nodes[i]->nodesLen = 0;
       nTree->nodes[i]->nodes = NULL;
@@ -238,6 +260,7 @@ t_syntaxTree* minInf(t_syntaxTree* tree, char* var) {
     else if (strcmp("=", nTree->nodes[i]->nodeName) == 0) {
       for (int j=0; j<nTree->nodes[i]->nodesLen; j++)
 	recFree(nTree->nodes[i]->nodes[j]);
+
       free(nTree->nodes[i]->nodes);
       nTree->nodes[i]->nodesLen = 0;
       nTree->nodes[i]->nodes = NULL;
@@ -247,6 +270,7 @@ t_syntaxTree* minInf(t_syntaxTree* tree, char* var) {
 
   return nTree;
 }
+
 
 void eval(t_syntaxTree* tree, char* var, t_syntaxTree* val) {
   for (int i=0; i<tree->nodesLen; i++) {
@@ -265,8 +289,10 @@ int calcm(t_syntaxTree* tree, char* var) {
 
   for(int i=0; i<tree->nodesLen; i++) {
     if(strcmp(tree->nodes[i]->nodeName, "div") == 0) {
+
       if(strcmp(tree->nodes[i]->nodes[0]->nodeName, var) == 0)
 	m = lcm(m, atoi(tree->nodes[i]->nodes[1]->nodeName));
+
       else if(strcmp(tree->nodes[i]->nodes[0]->nodeName, "+") == 0) {
 	for(int j=0; j<tree->nodes[i]->nodes[0]->nodesLen; j++) {
 	  if (strcmp(tree->nodes[i]->nodes[0]->nodes[j]->nodeName, var) == 0) {
@@ -281,6 +307,7 @@ int calcm(t_syntaxTree* tree, char* var) {
   return m;
 }
 
+
 t_syntaxTree* boundaryPoints(t_syntaxTree* tree, char* var) {
   char str[16];
   str[0] = '\0';
@@ -289,10 +316,10 @@ t_syntaxTree* boundaryPoints(t_syntaxTree* tree, char* var) {
   strcpy(bPoints->nodeName, "bPoints"); //useful only for debugging purposes
   bPoints->nodesLen = 0;
 
-
   for(int i=0; i<tree->nodesLen; i++) {
     if (strcmp(tree->nodes[i]->nodeName, "=") == 0) {
       t_syntaxTree** addends = tree->nodes[i]->nodes[0]->nodes;
+
       for (int j=0; j<tree->nodes[i]->nodes[0]->nodesLen; j++) {
 	if (strcmp(var, addends[j]->nodeName) == 0) {
 	  bPoints->nodesLen++;
@@ -301,6 +328,7 @@ t_syntaxTree* boundaryPoints(t_syntaxTree* tree, char* var) {
 	  bp->nodes = NULL;
 	  strcpy(bp->nodeName, "+");
 	  bp->nodesLen = 0;
+
 	  for (int k=0; k<tree->nodes[i]->nodes[0]->nodesLen; k++) {
 	    if (strcmp(var, addends[k]->nodeName) != 0) {
 	      bp->nodesLen++;
@@ -310,6 +338,7 @@ t_syntaxTree* boundaryPoints(t_syntaxTree* tree, char* var) {
 	      strcpy(bp->nodes[bp->nodesLen-1]->nodes[0]->nodeName, str);
 	    }
 	  }
+
 	  bp->nodesLen++;
 	  bp->nodes = realloc(bp->nodes, sizeof(t_syntaxTree*) * bp->nodesLen);
 	  bp->nodes[bp->nodesLen-1] = malloc(sizeof(t_syntaxTree));
@@ -326,6 +355,7 @@ t_syntaxTree* boundaryPoints(t_syntaxTree* tree, char* var) {
 
     if (strcmp(tree->nodes[i]->nodeName, ">") == 0) {
       t_syntaxTree** addends = tree->nodes[i]->nodes[0]->nodes;
+
       for (int j=0; j<tree->nodes[i]->nodes[0]->nodesLen; j++) {
 	if (strcmp(var, addends[j]->nodeName) == 0) {
 	  bPoints->nodesLen++;
@@ -334,6 +364,7 @@ t_syntaxTree* boundaryPoints(t_syntaxTree* tree, char* var) {
 	  bp->nodes = NULL;
 	  strcpy(bp->nodeName, "+");
 	  bp->nodesLen = 0;
+
 	  for (int k=0; k<tree->nodes[i]->nodes[0]->nodesLen; k++) {
 	    if (strcmp(var, addends[k]->nodeName) != 0) {
 	      bp->nodesLen++;
@@ -343,6 +374,7 @@ t_syntaxTree* boundaryPoints(t_syntaxTree* tree, char* var) {
 	      strcpy(bp->nodes[bp->nodesLen-1]->nodes[0]->nodeName, str);
 	    }
 	  }
+
 	  bp->nodesLen++;
 	  bp->nodes = realloc(bp->nodes, sizeof(t_syntaxTree*) * bp->nodesLen);
 	  bp->nodes[bp->nodesLen-1] = malloc(sizeof(t_syntaxTree));
@@ -360,6 +392,7 @@ t_syntaxTree* boundaryPoints(t_syntaxTree* tree, char* var) {
 
   return bPoints;
 }
+
 
 t_syntaxTree* newFormula(t_syntaxTree* tree, t_syntaxTree* minf, char* var) {
   int m = calcm(minf, var);
@@ -403,8 +436,8 @@ t_syntaxTree* newFormula(t_syntaxTree* tree, t_syntaxTree* minf, char* var) {
 
   recFree(bPts);
   return nTree;
-
 }
+
 
 void simplify(t_syntaxTree* t) {
   if (t->nodesLen != 0) {
@@ -443,9 +476,9 @@ void simplify(t_syntaxTree* t) {
     if (!simplified)
       for(int i=0; i<t->nodesLen; i++)
 	simplify(t->nodes[i]);
-    
   }
 }
+
 
 int recTreeToStr(t_syntaxTree* t, char** str, int len) {
   if (t->nodesLen == 0) {
@@ -454,6 +487,7 @@ int recTreeToStr(t_syntaxTree* t, char** str, int len) {
     strcat(*str, t->nodeName);
     return nLen;
   }
+
   else {
     int nLen = len + strlen(t->nodeName) + 1;
     *str = realloc(*str, sizeof(char) * nLen);
@@ -475,6 +509,7 @@ int recTreeToStr(t_syntaxTree* t, char** str, int len) {
   }
 }
 
+
 char* treeToStr(t_syntaxTree* tree) {
   char* str=malloc(sizeof(char));
   str[0] = '\0';
@@ -482,40 +517,20 @@ char* treeToStr(t_syntaxTree* tree) {
   return str;
 }
 
+
 char* cooper(char* wff, char* var) {
+  t_syntaxTree* tree, *minf, *f;
+  char* str;
 
-  t_syntaxTree* tree = parse(wff);
-
-  //char* str;
-  char* str = treeToStr(tree);
-  //printf("\n\n----Ingresso----\n%s", str);
-  free(str);
-
-  normalize(tree, var);
-  str = treeToStr(tree);
-  //printf("\n\n----Normalizzato----\n%s", str);
-  free(str);
-
-  t_syntaxTree* minf = minInf(tree, var);
-  str = treeToStr(minf);
-  //printf("\n\n----Minf----\n%s", str);
-  free(str);
-
-  t_syntaxTree* bPts = boundaryPoints(tree, var);
-  str = treeToStr(bPts);
-  //printf("\n\n----Boundary points----\n%s", str);
-  free(str);
-
-  t_syntaxTree* f = newFormula(tree, minf, var);
-  simplify(f);
+  tree = parse(wff); //Genera l'albero sintattico a partire dalla stringa
+  normalize(tree, var); //Trasforma tree in $\varphi_{- \infty}$
+  minf = minInf(tree, var);
+  f = newFormula(tree, minf, var);
+  simplify(f); //opzionale
   str = treeToStr(f);
-  //printf("\n\n----Formula eq----\n%s", str);
-  //free(str);
 
-        
   recFree(tree);
   recFree(minf);
-  recFree(bPts);
   recFree(f);
 
   return str;
